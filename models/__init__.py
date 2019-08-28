52e87ce357460628fcc5060b08bdbb286b9aa5fd
Jeff Lyon <jeff@rubbingalcoholic.com>
2019-08-27 20:16:14 -0500

    graceful fail on db autocreation failure

--- models/__init__.py
+++ models/__init__.py
@@ -13,15 +13,19 @@ def initialize_database(app):
     import os
 
     with app.app_context():
-        db.create_all()
-        account = Account.query.get(0)
-        if not account:
-            # insert guest account if needed.
-            conn    = psycopg2.connect(os.environ.get('DATABASE_URI'))
-            query   = ("INSERT INTO account (id, email, password, api_key) "
-                       "VALUES (%s, %s, %s, %s);")
-            data    = (0, 'anon@faxrobot.internal', 'trol', 'lol')
-            cursor  = conn.cursor()
-            cursor.execute(query, data)
-            conn.commit()
-            conn.close()
+        try:
+            db.create_all()
+            account = Account.query.get(0)
+            if not account:
+                # insert guest account if needed.
+                conn    = psycopg2.connect(os.environ.get('DATABASE_URI'))
+                query   = ("INSERT INTO account (id, email, password, api_key) "
+                           "VALUES (%s, %s, %s, %s);")
+                data    = (0, 'anon@faxrobot.internal', 'trol', 'lol')
+                cursor  = conn.cursor()
+                cursor.execute(query, data)
+                conn.commit()
+                conn.close()
+        except:
+            print("ERROR: COULD NOT AUTO-GENERATE DATABASE TABLES")
+            print("Make sure your SQLALCHEMY_DATABASE_URI environment is set!")
